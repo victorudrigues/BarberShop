5dfdd7b667e42b466eb7bf93648878ea7621c566
Victorodrigues <viictorhgoliveira@gmail.com>
2024-01-31 15:27:49 -0300

    feat: formatação de data com date-fns

--- barbershop/app/(home)/page.tsx
+++ barbershop/app/(home)/page.tsx
@@ -1,11 +1,22 @@
-import Image from "next/image";
+
 import Header from "../_components/header";
+import { format } from "date-fns"
+import { ptBR } from "date-fns/locale";
 
 
 export default function Home() {
   return (
-   <>
-   <Header></Header>
-   </>
+    <>
+      <Header />
+
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold"> Olá, Victor Hugo!</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR,
+          })}</p>
+      </div>
+
+    </>
   );
 }
