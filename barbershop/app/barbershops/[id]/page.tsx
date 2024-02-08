6c14a3ff132e3f35bfb7b66e09cf444d4ef09e4a
Victorodrigues <viictorhgoliveira@gmail.com>
2024-02-08 11:09:45 -0300

    feat: criação do componente barbershop info

--- barbershop/app/barbershops/[id]/page.tsx
+++ barbershop/app/barbershops/[id]/page.tsx
@@ -1,32 +1,40 @@
+import { Button } from "@/app/_components/ui/button";
 import { db } from "@/app/_lib/prisma";
+import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
+import Image from "next/image";
+import BarberShopInfo from "./_components/barbershop-info";
 
 
 interface BarberShopDetailsPageProps {
-    params:{
+    params: {
         id?: string;
     };
 }
 
-const BarberShopDetailsPage = async ({params}: BarberShopDetailsPageProps) => {
+const BarberShopDetailsPage = async ({ params }: BarberShopDetailsPageProps) => {
 
-    if(!params){
+    if (!params) {
         // TODO: Retorna a home page
         return null;
     }
 
     const barbershop = await db.barbershop.findUnique({
-        
+
         where: {
             id: params.id,
         }
     });
-    
-    if(!barbershop){
+
+    if (!barbershop) {
         // TODO: retorna a Home Page
         return null;
     }
 
-    return <h1> {barbershop.name}</h1>
+    return (
+        <>
+           <BarberShopInfo barbershop={barbershop}/>
+        </>
+    )
 }
 
 export default BarberShopDetailsPage;
