ab71cf06acdc0f61fcb4c7aa03ecf741bb4f7fba
Victorodrigues <viictorhgoliveira@gmail.com>
2024-02-19 16:39:01 -0300

    feat: next auth google

--- barbershop/app/api/auth/[...nextauth]/route.ts
+++ barbershop/app/api/auth/[...nextauth]/route.ts
@@ -9,11 +9,10 @@ const handler = NextAuth({
     adapter: PrismaAdapter(db) as Adapter,
     providers: [
         GoogleProvider({
-        clientId: "",
-        clientSecret:"",
+        clientId: process.env.GOOGLE_CLIENT_ID as string,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
     ],
-    
 })
 
 export { handler as GET, handler as POST }
