aea34ed658f7e0d1367ffb6e6543bc96a3b732f7
Victorodrigues <viictorhgoliveira@gmail.com>
2024-03-16 13:27:26 -0300

    feat: adicionando conteudo do menu em barbershop-item

--- barbershop/app/barbershops/[id]/_components/barbershop-info.tsx
+++ barbershop/app/barbershops/[id]/_components/barbershop-info.tsx
@@ -1,6 +1,8 @@
 "use client"
 
+import { SideMenu } from "@/app/_components/side-menu";
 import { Button } from "@/app/_components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/app/_components/ui/sheet";
 import { Barbershop } from "@prisma/client";
 import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
 import Image from "next/image";
@@ -10,7 +12,7 @@ interface BarberShopInfoProps {
     barbershop: Barbershop;
 }
 
-const BarberShopInfo = ({barbershop}: BarberShopInfoProps) => {
+const BarberShopInfo = ({ barbershop }: BarberShopInfoProps) => {
     const router = useRouter();
     //Voltar a página anterior
     const handleBackClick = () => {
@@ -26,7 +28,21 @@ const BarberShopInfo = ({barbershop}: BarberShopInfoProps) => {
                 </Button>
 
                 <Button size="icon" variant="outline" className="z-50 absolute top-4 right-4">
-                    <MenuIcon />
+                    <Sheet>
+                        <SheetTrigger asChild>
+                            <Button variant={"outline"} size="icon">
+                                <MenuIcon size={16} />
+                            </Button>
+                        </SheetTrigger>
+
+                        <SheetContent className="p-0">
+
+                            {/* conteudo menu */}
+                            <SideMenu />
+
+                        </SheetContent>
+                    </Sheet>
+
                 </Button>
 
                 <Image
